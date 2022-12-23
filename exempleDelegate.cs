0f8d88db71bb8d5480d17042af2258ef491b894b
Simon Beland <2067773@cegepoutaouais.qc.ca>
2022-12-22 21:49:25 -0500

    Ajout Delegate 2

--- exempleDelegate.cs
+++ exempleDelegate.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace appWin_liguev2
 {
-    internal class Class1
+    internal class exempleDelegate
     {
+        public delegate void Delegate1(string text);
+
+        public Delegate1 exemple;
+
+
+        public void Salutation(string nom)
+        {
+            Console.WriteLine("Bonjour" + nom);
+        }
+
+        public void Aurevoir(string nom) 
+        {
+            Console.WriteLine("Au revoir" + nom);
+        }
+
+        
+
+        public void Main()
+        {
+            exemple = Salutation;
+            exemple += Aurevoir;
+
+            exemple("Simon");
+
+            
+            Delegate1 message = (string text) => { Console.WriteLine(text); };
+
+            message("Ceci est un message");
+
+        }
     }
 }
